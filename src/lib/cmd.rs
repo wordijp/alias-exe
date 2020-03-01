61101141683586f03bf7c957e0da753c3460efb2
wordi <wordijp@gmail.com>
2020-03-01 14:34:01 +0900

    Split argument, support arg: "space contain text"

--- src/lib/cmd.rs
+++ src/lib/cmd.rs
@@ -6,9 +6,11 @@ use crate::lib::encode;
 use crate::lib::term;
 
 pub fn command_output(cmd: &str) -> io::Result<String> {
+    let args = split_args(cmd);
+
     let output = Command::new("cmd")
         .arg("/c")
-        .arg(cmd)
+        .args(args)
         .output()?;
 
     if !output.status.success() {
@@ -18,9 +20,11 @@ pub fn command_output(cmd: &str) -> io::Result<String> {
 }
 
 pub fn command_spawn(cmd: &str) -> io::Result<()> {
+    let args = split_args(cmd);
+
     let mut cmd: Child = Command::new("cmd")
         .arg("/c")
-        .arg(cmd)
+        .args(args)
         .spawn()?;
     let status = cmd.wait()?;
 
@@ -28,4 +32,48 @@ pub fn command_spawn(cmd: &str) -> io::Result<()> {
         return Err(Error::new(ErrorKind::Interrupted, format!("{}: {}", term::ewrite("failed")?, status.to_string())));
     }
     Ok(())
+}
+
+fn split_args(cmd: &str) -> Vec<String> {
+    let mut args = Vec::new();
+
+    let mut s = String::new();
+    let mut escape = false;
+    let mut double_quote = false;
+
+    for ch in cmd.split("") {
+        match ch {
+            " " => {
+                if double_quote {
+                    s.push_str(ch);
+                } else if s.len() > 0 {
+                    args.push(s.clone());
+                    s.clear();
+                }
+            },
+            "\\" => {
+                if escape {
+                    s.push_str("\\");
+                    escape = false;
+                } else {
+                    escape = true;
+                }
+            },
+            "\"" => {
+                if escape {
+                    s.push_str("\"");
+                    escape = false;
+                } else {
+                    double_quote = !double_quote;
+                }
+            },
+            _ => s.push_str(ch),
+        }
+    }
+
+    if s.len() > 0 {
+        args.push(s);
+    }
+
+    args
 }
